d6e2e64c05f2cdd95908f33c38f75ca6a876363e
zita-huang <vm@usna.edu m272898@usna.edu>
2025-01-16 14:03:52 -0500

    msg

---
+++ class/types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    int main ()
+    {
+        int x, y, result; 
+        x = 3;
+        y = 5;
+
+    result = x/y;
+    cout << "x/y is: " << result << endl;
+
+    return 0;
+    }
