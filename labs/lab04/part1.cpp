97fd76081cd28eeed925de51721f2c56e4f7ddaf
zita-huang <m272898@usna.edu>
2025-01-30 15:10:54 -0500

    msg

---
+++ labs/lab04/part1.cpp
@@ -0,0 +1,44 @@
+/*******************
+Filename: part1.cpp
+Author: Zita Huang (m272898)
+Lab04 Part 1
+********************/
+#include <iostream>
+#include <stream>
+#include <fstream>
+using namespace std;
+
+    int main()
+    {
+        string file;
+        cin >> file;
+
+        ifstream fin(file);
+
+        if(!fin)
+        {
+            cout << "Could not open file '" << file << "'" << endl;
+            return 1;
+        }
+
+        double temp, count, Fsum;
+        Fsum = 0.0;
+        count = 0.0;
+
+        string time;
+
+        while (fin >> time >> temp)
+        {  
+            double Fah = (9.0/5.0)*(temp)+32.0;
+
+            Fsum = Fah + Fsum;
+            count = count + 1.0;
+        }
+        if(count > 0)
+        {
+            cout << "file: " << file << endl;
+            cout << "ave: " << Fsum/count << endl;
+        }
+
+        return 0;
+    }
