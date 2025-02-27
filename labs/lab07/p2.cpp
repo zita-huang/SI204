fe0515b3fde9d5a61086324ea41bc49cf36be6d7
zita-huang <m272898@usna.edu>
2025-02-27 15:15:24 -0500

    msg

---
+++ labs/lab07/p2.cpp
@@ -0,0 +1,64 @@
+/****************
+Filename: p2.cpp
+Author: Zita Huang(m272898)
+Lab07 Part 2
+*****************/
+#include <iostream>
+using namespace std;
+
+    int* read(int N);
+    int print(int* A, int N);
+    bool isInOrder(int* A, int N);
+    int 
+
+    int main(){
+        cout << "Welcome to SWARE!" << endl;
+        char c;
+        int N;
+        cin >> c >> c >> N >> c;
+
+        int* A = read(N);
+        print(A,N);
+         if(isInOrder(A,N))
+            cout << "Is in order!" << endl;
+        else
+            cout << "Is not in order!" << endl;
+        
+        delete[] A;
+        return 0;
+    }
+
+    int* read(int N){
+        int* array = new int [N];
+        for(int i = 0; i < N; i++){
+            cin >> array[i];
+        }
+        return array;
+    }
+
+    int print(int* A, int N){
+        int dash = 0;
+        for( int i = 0; i < N; i++){
+            cout << A[i] << " ";
+            dash = N * 2 + 1;
+        }
+        cout << endl;
+        for(int i = 0; i < dash; i++){
+            cout << "-";
+        }
+        cout << endl;
+        for(int i = 0; i < N; i++){
+            cout << char('A' + i) << " ";
+        }
+        cout << endl;
+        return N;
+    }
+
+    bool isInOrder(int* A, int N){
+        for(int i = 1; i < N ; i++){
+            if(A[i] < A[i-1]){
+                return false;
+            }
+        } 
+        return true;
+    }
