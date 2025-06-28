e22275e8d2fce9d0ef1b6128e6cacb26e5d5be84
zita-huang <milkywayandkitkats@gmail.com>
2025-06-28 15:10:55 -0400

    Add files via upload

---
+++ practicum/6wkp/p2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+using namespace std;
+    int main()
+    {
+        string s, space, space2, s2;
+        int num1 , num2;
+        cin >> s >> space >> num1 >> space2 >> num2 >> s2;
+
+        if(s == "Original:")
+        {
+            cout << space << space2 << endl;
+        }
+
+        return 0;
+    }
